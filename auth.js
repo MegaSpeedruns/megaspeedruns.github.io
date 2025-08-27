64cd7bc1342fd64f236410efae3aa092c391f8a1
MegaSpeedruns <megaspeedrunning@gmail.com>
2025-08-27 07:59:03 +0100

    Add files via upload

--- auth.js
+++ auth.js
@@ -4,9 +4,14 @@ firebase.initializeApp({
   authDomain: "megaspeedrunsdatabase.firebaseapp.com",
   databaseURL: "https://megaspeedrunsdatabase-default-rtdb.firebaseio.com",
   projectId: "megaspeedrunsdatabase",
-  storageBucket: "megaspeedrunsdatabase.appspot.com", // <-- FIXED
+  storageBucket: "megaspeedrunsdatabase.appspot.com",
   messagingSenderId: "545413984550",
   appId: "1:545413984550:web:79c899026a21f6343091e7"
 });
 
 const auth = firebase.auth();
+const db = firebase.database();
+
+// Enable login persistence across the entire domain
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .catch(err => console.error("Persistence error:", err));
